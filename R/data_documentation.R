3bf9ea453f0d135d968781f5267733c797adeec4
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-05-21 14:27:34 +0200

    Documented datasets

---
+++ R/data_documentation.R
@@ -0,0 +1,22 @@
+#' Monthly Sunspot Data, 1749-2012
+#'
+#' Modified version of the base \link[datasets]{sunspots} dataset.
+#' Includes columns for year, month and start_date of the month.
+#' Used for examples.
+#'
+#' @name sunspots_
+#' @docType data
+#' @source Based on \link[datasets]{sunspot.month}
+#' @keywords data
+NULL
+
+#' First 44 US Presidents
+#'
+#' List of presidents. Includes date of inauguration, party and home state.
+#'
+#' @name USA.presidents
+#' @docType data
+#' @source Based on
+#'  \url{http://thedatahub.org/api/data/ba0cdb03-c0f0-45ff-a21f-63fdf6ce1a89/}
+#' @keywords data
+NULL
